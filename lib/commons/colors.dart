a87716c459fa6c8d9cacf4ab5fe4a5d20500241c
Ibekason Alexander <alibekason@gmail.com>
2021-05-29 12:55:49 +0100

    Add color constants

---
+++ lib/commons/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const kDeepBackgroundColor = Color(0xff16171B);
+const kBackgroundColor = Color(0xff353A40);
+const kStrokeColor = Color(0xff424750);
+const kLinearDeepBlue = Color(0xff005EA3);
+const kLinearLightBlue = Color(0xff11A8FD);
+const kAppbarButtonLinearStart = Color(0xff16171B);
+const kAppbarButtonLinearEnd = Color(0xff31343C);
+const kGreyTextColor = Color(0xff7F8489);
+const kWhiteTextColor = Color(0xffFDFDFD);
